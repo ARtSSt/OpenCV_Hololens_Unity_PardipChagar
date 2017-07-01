b79f5c2f8c4988d9801ff8abfc5dbc247782693b
Pardip Singh Chagar <pardipchagar@gmail.com>
2017-06-30 20:35:12 -0700

    Organized Repo Push

---
+++ HoloLensCameraStream-master/HoloLensCameraStream-master/HoloLensVideoCaptureExample/Assets/CamStream/Examples/Video Panel Example/Scripts/VideoPanel.cs
@@ -0,0 +1,57 @@
+﻿//  
+// Copyright (c) 2017 Vulcan, Inc. All rights reserved.  
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+//
+
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.IO;
+using System;
+using System.Collections;
+
+public class VideoPanel : MonoBehaviour
+{
+    public RawImage rawImage;
+
+    public void SetResolution(int width, int height)
+    {
+        var texture = new Texture2D(width, height, TextureFormat.BGRA32, false);
+        rawImage.texture = texture;
+    }
+
+    public void SetBytes(byte[] image)
+    {
+
+        var texture = rawImage.texture as Texture2D;
+        texture.LoadRawTextureData(image); //TODO: Should be able to do this: texture.LoadRawTextureData(pointerToImage, 1280 * 720 * 4);
+        texture.Apply();
+    }
+
+    public void SaveFrame(byte[] image, int numFrames)
+    {
+        if (numFrames < 60)
+        {
+            var texture = new Texture2D(896, 504, TextureFormat.BGRA32, false);
+            //texture.LoadRawTextureData(image);
+            //texture.Apply();
+            //byte[] bytes = texture.EncodeToPNG();
+            //string path = Path.Combine(Application.persistentDataPath, "HoloCapture" + numFrames + ".png");
+            //FileStream stream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+            //BinaryWriter writer = new BinaryWriter(stream);
+            //for (int i = 0; i < bytes.Length; i++)
+            //{
+            //    writer.Write(bytes[i]);
+            //}
+            //writer.Close();
+            //stream.Dispose();
+        }
+    }
+
+    public void SendOutFrame (byte[] frame, int limit)
+    {
+        string data = Convert.ToBase64String(frame); 
+        UnityWebRequest www = UnityWebRequest.Put("http://100.80.230.214/~pardipchagar/sunisshining/file.php", data);
+        www.Send();
+    }
+}
---
+++ HoloLensCameraStream-master/HoloLensCameraStream-master/HoloLensVideoCaptureExample/Assets/CamStream/Examples/Video Panel Example/Scripts/VideoPanelApp.cs
@@ -0,0 +1,152 @@
+﻿//  
+// Copyright (c) 2017 Vulcan, Inc. All rights reserved.  
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+//
+
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System.IO;
+using HoloLensCameraStream;
+using HololensIO;
+
+/// <summary>
+/// This example gets the video frames at 30 fps and displays them on a Unity texture,
+/// which is locked the User's gaze.
+/// </summary>
+public class VideoPanelApp : MonoBehaviour
+{
+
+    static FileIO _file;
+    byte[] pixelOutBuffer;
+
+    GCHandle pixelInBufferPtr;
+    GCHandle pixelOutBufferPtr;
+    bool bufferCreated = false;
+    static int framesSaved;
+    bool runOnce;
+
+    [DllImport("HoloPlugin", EntryPoint = "DetectContours")]
+    public static extern int DetectContours(byte[] inPixels, int w, int h, byte[] outPixels);
+
+    byte[] _latestImageBytes;
+    HoloLensCameraStream.Resolution _resolution;
+
+    //"Injected" objects.
+    VideoPanel _videoPanelUI;
+    VideoCapture _videoCapture;
+
+    void Start()
+    {
+        framesSaved = 0;
+        runOnce = true;
+        //Call this in Start() to ensure that the CameraStreamHelper is already "Awake".
+        CameraStreamHelper.Instance.GetVideoCaptureAsync(OnVideoCaptureCreated);
+        //You could also do this "shortcut":
+        //CameraStreamManager.Instance.GetVideoCaptureAsync(v => videoCapture = v);
+        _videoPanelUI = GameObject.FindObjectOfType<VideoPanel>();
+    }
+
+    private void OnDestroy()
+    {
+        if (_videoCapture != null)
+        {
+            _videoCapture.FrameSampleAcquired -= OnFrameSampleAcquired;
+            _videoCapture.Dispose();
+        }
+    }
+
+    void OnVideoCaptureCreated(VideoCapture videoCapture)
+    {
+        if (videoCapture == null)
+        {
+            Debug.LogError("Did not find a video capture object. You may not be using the HoloLens.");
+            return;
+        }
+        
+        this._videoCapture = videoCapture;
+
+        _resolution = CameraStreamHelper.Instance.GetLowestResolution();
+        float frameRate = CameraStreamHelper.Instance.GetHighestFrameRate(_resolution);
+        videoCapture.FrameSampleAcquired += OnFrameSampleAcquired;
+
+        CameraParameters cameraParams = new CameraParameters();
+        cameraParams.cameraResolutionHeight = _resolution.height;
+        cameraParams.cameraResolutionWidth = _resolution.width;
+        cameraParams.frameRate = Mathf.RoundToInt(frameRate);
+        cameraParams.pixelFormat = CapturePixelFormat.BGRA32;
+
+        UnityEngine.WSA.Application.InvokeOnAppThread(() => {
+            _videoPanelUI.SetResolution(_resolution.width, _resolution.height);
+        }, false);
+
+        videoCapture.StartVideoModeAsync(cameraParams, OnVideoModeStarted);
+    }
+
+    void OnVideoModeStarted(VideoCaptureResult result)
+    {
+        if (result.success == false)
+        {
+            Debug.LogWarning("Could not start video mode.");
+            return;
+        }
+
+        Debug.Log("Video capture started.");
+    }
+
+    void OnFrameSampleAcquired(VideoCaptureSample sample)
+    {
+        //When copying the bytes out of the buffer, you must supply a byte[] that is appropriately sized.
+        //You can reuse this byte[] until you need to resize it (for whatever reason).
+        if (_latestImageBytes == null || _latestImageBytes.Length < sample.dataLength 
+            || pixelOutBuffer == null || pixelOutBuffer.Length < sample.dataLength)
+        {
+            _latestImageBytes = new byte[sample.dataLength];
+            pixelOutBuffer = new byte[sample.dataLength];
+        }
+
+        sample.CopyRawImageDataIntoBuffer(_latestImageBytes);
+        sample.Dispose();
+
+        //framesSaved++;
+        //if (framesSaved < 29)
+        //{
+        //    UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+        //    {
+        //        _videoPanelUI.SaveFrame(_latestImageBytes, framesSaved);
+        //    }, true);
+        //}
+
+        if (bufferCreated)
+        {
+            pixelInBufferPtr.Free();
+            pixelOutBufferPtr.Free();
+            bufferCreated = false;
+        }
+
+        pixelInBufferPtr = GCHandle.Alloc(_latestImageBytes, GCHandleType.Pinned);
+        pixelOutBufferPtr = GCHandle.Alloc(pixelOutBuffer, GCHandleType.Pinned);
+
+        DetectContours(_latestImageBytes, 896, 504, pixelOutBuffer);
+        bufferCreated = true;
+
+        if (bufferCreated)
+        {
+            pixelInBufferPtr.Free();
+            pixelOutBufferPtr.Free();
+            bufferCreated = false;
+        }
+
+        //This is where we actually use the image data
+        UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+        {
+            _videoPanelUI.SetBytes(pixelOutBuffer);
+            if (runOnce)
+            {
+                _videoPanelUI.SendOutFrame(pixelOutBuffer, 0);
+                runOnce = false;
+            }
+        }, false);
+
+        
+    }
+}
